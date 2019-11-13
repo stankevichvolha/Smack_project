87571fe59c557383dabd98b38a7ede474c0d27db
Volha Stankevich <stankevichvolhaig@gmail.com>
2019-11-13 14:22:41 -0800

    project setup

---
+++ Podfile
@@ -0,0 +1,15 @@
+project 'Smack_OlgaST.xcodeproj'
+
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Smack_OlgaST' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Smack_OlgaST
+  pod 'Alamofire'
+  pod 'SwiftyJSON'
+  pod 'Socket.IO-Client-Swift'
+
+end
---
+++ Smack_OlgaST/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Smack_OlgaST
+//
+//  Created by Volha  Stankevich on 11/13/19.
+//  Copyright © 2019 Volha  Stankevich. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
